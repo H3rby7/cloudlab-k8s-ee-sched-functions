054777de3ed615c30e70419d9a61f87da1df7839
h3rby7 <herbielutz@gmx.net>
2025-02-02 23:11:56 +0100

    RM Colosseum.py; rm sleep-stress from Loader.py

--- Loader.py
+++ Loader.py
@@ -1,9 +1,11 @@
 import random
-import time
 import os
 from concurrent.futures import ThreadPoolExecutor, wait
 import jsonmerge
 import string
+import logging
+
+logger = logging.getLogger(__name__)
 
 params_processed = False
 params = dict()
@@ -31,26 +33,26 @@ def cpu_loader_job(params):
         # print(f"Value: 3.{''.join(pi_greco[1:])}\n")
 
 def cpu_loader(params):
-    # print("--------> CPU stress start")
+    logging.debug("CPU stress start")
     pool_size = int(params["thread_pool_size"])
     pool = ThreadPoolExecutor(pool_size)
     futures = list()
     for thread in range(pool_size):
         futures.append(pool.submit(cpu_loader_job, params))
     wait(futures)
-    # print("--------> CPU stress test stop")
+    logging.debug("CPU stress test stop")
     return
 
 def bandwidth_loader(params):
-    # print("--------> Network stress start")
+    logging.debug("Network stress start")
     bandwidth_load = random.expovariate(1 / params["mean_response_size"])
     num_chars = int(max(1, 1000 * bandwidth_load))  # Response in kB
     response_body = ''.join(random.choice(string.ascii_letters) for i in range(num_chars))
-    # print("--------> Network stress stop")
+    logging.debug("Network stress stop")
     return response_body
 
 def memory_loader(params):
-    # print("--------> Memory stress start")
+    logging.debug("Memory stress start")
     memory_size = params["memory_size"]
     memory_io = params["memory_io"]
     
@@ -61,12 +63,11 @@ def memory_loader(params):
     for i in range(0, int(memory_io)):
         v = dummy_buffer[i % int(memory_size)]  # read operation
         dummy_buffer[i % int(memory_size)] = ['A' * 1000] # write operation
-    # print("--------> Memory stress stop")
+    logging.debug("Memory stress stop")
     return dummy_buffer
 
 def disk_loader(params):
-        # print("--------> Disk stress start")
-        # print("--------> Write stress start")
+        logging.debug("Disk stress - Write stress start")
         filename_base = params["tmp_file_name"]
         rnd_str = ''.join(random.choice(string.ascii_lowercase) for i in range(10))
         filename = f"{rnd_str}-{filename_base}"
@@ -78,9 +79,9 @@ def disk_loader(params):
             os.write(f, buff)
         os.fsync(f)  # force write to disk
         os.close(f)
-        # print("--------> Write stress stop")
+        logging.debug("Disk stress - Write stress stop")
 
-        # print("--------> Read stress start")
+        logging.debug("Disk stress - Read stress start")
         f = os.open(filename, os.O_RDONLY, 0o777)  # low-level I/O
         # generate random read positions
         offsets = list(range(0, blocks_count * block_size, block_size))
@@ -91,15 +92,9 @@ def disk_loader(params):
             buff = os.read(f, block_size)  # read from position
             if not buff: break  # if EOF reached
         os.close(f)
-        # print("--------> Read stress stop")
+        logging.debug("Disk stress - Read stress stop")
         os.remove(filename)
         return
-
-def sleep_loader(params):
-    # print("--------> Sleep start")
-    time.sleep(float(params["sleep_time"]))
-    # print("--------> Sleep stop")
-    return
 
 def loader(input_params):
     global params_processed, params
@@ -109,7 +104,6 @@ def loader(input_params):
             "cpu_stress": {"run":False,"range_complexity": [100, 100], "thread_pool_size": 1, "trials": 1},
             "memory_stress":{"run":False, "memory_size": 10000, "memory_io": 1000},
             "disk_stress":{"run":False,"tmp_file_name":  "mubtestfile.txt", "disk_write_block_count": 1000, "disk_write_block_size": 1024},
-            "sleep_stress":{"run":True,"sleep_time": 0.01},
             "mean_response_size": 11}
 
         params = jsonmerge.merge(default_params,input_params)
@@ -123,8 +117,6 @@ def loader(input_params):
         memory_loader(params['memory_stress'])
     if params['disk_stress']['run']:
         disk_loader(params['disk_stress'])
-    if params['sleep_stress']['run']:
-        sleep_loader(params['sleep_stress'])
     return bandwidth_loader(params)
 
 if __name__ == '__main__':
--- README.md
+++ README.md
@@ -2,29 +2,6 @@
 
 Excerpt of the [µBench repository](https://github.com/mSvcBench/muBench/tree/main/CustomFunctions)
 
-## Colosseum
-
-Function that returns a simple asci art of Colosseum
-
-*Function name*: `colosseum`
-
-*Input Paramenters*: none
-
-## ComputePI
-
-This *single-thread* internal function (included in the service-cell code) stresses the CPU by computing `D` decimal points of pi, where `D` is a random integer in `range_complexity` (e.g. 50,100). The return is a string whose length is a sample of an exp neg random variable with mean `mean_response_size` kBytes  
-
-*Function name*: `compute_pi`
-
-*Default Input Paramenters*:
-
-```json
-{
-    "range_complexity": [50, 100], 
-    "mean_response_size": 10
-}
-```
-
 ## Loader
 
 This *multi-thread* internal function sequentially load the CPU, the memory, the disk and then sleep for a bit. Finally, it returns a string whose length is a sample of an exp neg random variable with mean `mean_response_size` kBytes.
@@ -35,7 +12,6 @@ The memory stress is performed (if `run`=true) by allocating `memory_size` kByte
 
 The disk stress is performed by creating the file `tmp_file_name`. Writing `disk_write_block_count` blocks of  `disk_write_block_size` bytes. Finally, random accessing each of them once.
 
-The sleep stress is performed (if `run`=true) by sleeping for `sleep_time` seconds.
 
 *Function name*: `loader`
 
@@ -43,23 +19,20 @@ The sleep stress is performed (if `run`=true) by sleeping for `sleep_time` secon
 
 ```json
 {
-        "cpu_stress": {
-            "run":false,
-            "range_complexity": [100, 100], "thread_pool_size": 1, 
-            "trials": 1
-        },
-        "memory_stress":{
-            "run":false, 
-            "memory_size": 10000, "memory_io": 1000
-        },
-        "disk_stress":{
-            "run":false,
-            "tmp_file_name":  "mubtestfile.txt", 
-            "disk_write_block_count": 1000, "disk_write_block_size": 1024
-        },
-        "sleep_stress":{
-            "run":true,
-            "sleep_time":  0.01
-        }
-        "mean_response_size": 11}
+    "cpu_stress": {
+        "run":false,
+        "range_complexity": [100, 100], "thread_pool_size": 1, 
+        "trials": 1
+    },
+    "memory_stress":{
+        "run":false, 
+        "memory_size": 10000, "memory_io": 1000
+    },
+    "disk_stress":{
+        "run":false,
+        "tmp_file_name":  "mubtestfile.txt", 
+        "disk_write_block_count": 1000, "disk_write_block_size": 1024
+    },
+    "mean_response_size": 11
+}
 ```
